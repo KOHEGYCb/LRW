655490a12c4f78cf61dc5b907a650c114f2f8ccb
Unknown <dimaushenko@mail.ru>
2019-09-16 17:58:44 +0300

    for work pc

--- DBConnection/src/main/java/by/dmitry/lrw_dbconnection/connecttodb/Constants.java
+++ DBConnection/src/main/java/by/dmitry/lrw_dbconnection/connecttodb/Constants.java
@@ -5,5 +5,5 @@ package by.dmitry.lrw_dbconnection.connecttodb;
  * @author Dmitry
  */
 public class Constants {
-    public static String SDB_FILE_NAME = "C:\\Users\\Dmitry\\Documents\\NetBeansProjects\\LRW\\DBstr";
+    public static String SDB_FILE_NAME = "C:\\Users\\user\\Documents\\NetBeansProjects\\LRW\\DBstr";
 }
