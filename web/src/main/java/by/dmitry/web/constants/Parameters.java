90327c1ee3c6aa753d47ecf8dc5a734320e100aa
Dmitry <dimaushenko@mail.ru>
2019-09-17 17:24:55 +0300

    2

---
+++ web/src/main/java/by/dmitry/web/constants/Parameters.java
@@ -0,0 +1,14 @@
+package by.dmitry.web.constants;
+
+/**
+ *
+ * @author Dmitry
+ */
+public class Parameters {
+    public static final String SUB_NAME = "subName";
+    public static final String FIND_VAR = "findVar";
+    public static final String TABLE_NAME = "tabName";
+    public static final String SQL_COLUMN = "sqlColumn";
+    public static final String AMOUNT_COLS = "kolCols";
+    public static final String COL_N = "col";
+}
