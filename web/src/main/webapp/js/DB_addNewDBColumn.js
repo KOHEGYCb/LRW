9e1eb944fb97e97af1a4f8eb16fa4ce1430054b7
Dmitry <dimaushenko@mail.ru>
2019-10-09 16:04:52 +0300

    rafactor + new functions

--- web/src/main/webapp/js/DB_addNewDBColumn.js
+++ web/src/main/webapp/js/DB_addNewDBColumn.js
@@ -1,11 +1,4 @@
-var newInput = "" +
-        "<div class='colBlk'>" +
-        "<input type='text' value= '<VALUE>' class='col' name='col<ITER>' id='col<ITER>' placeholder='Column name <ITER1>' required/>" +
-        "<img src='img/cancel.svg' alt='X' onclick='delCol(\"col<ITER>\")' width='32' />" +
-        "</div>";
-
-var btn_addSQLCol = document.getElementById('addQSLCol');
-btn_addSQLCol.addEventListener('click', addSQLCol, false);
+/* global newInput */
 
 function addSQLCol() {
     var block = document.getElementById('sqlColomnBlock');
@@ -16,10 +9,12 @@ function addSQLCol() {
         str += setNewInput(document.getElementById('col' + i).value, i);
     str += setNewInput("", count);
     block.innerHTML = str;
-    document.getElementById('kolCols').value = count;
+    initSQLColEventListners();
+    updateColAmount();
+    updateSelectColQuestions();
 }
 
-function delCol(a) {
+function delSQLCol(a) {
     document.getElementById(a).parentElement.outerHTML = "";
     var block = document.getElementById('sqlColomnBlock');
     var count = document.getElementsByClassName('col').length;
@@ -28,7 +23,8 @@ function delCol(a) {
     for (var i = 0; i < count; i++)
         str += setNewInput(document.getElementsByClassName('col')[i].value, i);
     block.innerHTML = str;
-    document.getElementById('kolCols').value = count;
+    updateColAmount();
+    updateSelectColQuestions(a);
 }
 
 function setNewInput(value, iter) {
@@ -38,4 +34,55 @@ function setNewInput(value, iter) {
     str = str.replace(new RegExp("<ITER>", 'g'), iter);
     str = str.replace(new RegExp("<ITER1>", 'g'), iter + 1);
     return str;
+}
+
+function updateSelectColQuestions(id = "-1") {
+    var selects = document.getElementsByClassName("selectCol");
+    var SQLCols = document.getElementsByClassName("col");
+    var optStr = "";
+    if (id !== "-1") {
+        id = parseInt(id.substring(3)) + 1;
+        console.log("id: " + id);
+    }
+    for (var i = 0; i < SQLCols.length; i++) {
+        if (SQLCols[i].value === "") {
+            optStr += "<option>" + SQLCols[i].name + "</option>";
+        } else {
+            optStr += "<option>" + SQLCols[i].value + "</option>";
+        }
+    }
+    for (var i = 0; i < selects.length; i++) {
+        var n = selects[i].selectedIndex;
+        console.log("n: " + n);
+        var v = selects[i].value;
+        console.log("v: " + v);
+        
+        selects[i].innerHTML = optStr;
+        
+        if (n === id){
+            selects[i].selectedIndex = -1;
+        }
+        
+        
+//        for (var i = 0; i < SQLCols.length; i++) {
+//            if (SQLCols[i].value.length === 0) {
+//                v = 0;
+//            }
+//
+//            if (v.length > SQLCols[i].value.length) {
+//
+//            }
+//            console.log("v: " + v.length + " c: " + SQLCols[i].value.length);
+//        }
+}
+}
+
+function updateColAmount() {
+    document.getElementById('kolCols').value = document.getElementsByClassName('col').length - 1;
+}
+
+function initSQLColEventListners() {
+    for (var i = 0; i < document.getElementsByClassName('col').length; i++) {
+        document.getElementById("col" + i).addEventListener('keyup', updateSelectColQuestions, false);
+    }
 }
